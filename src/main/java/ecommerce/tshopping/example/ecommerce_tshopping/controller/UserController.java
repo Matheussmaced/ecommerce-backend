eea2bcfc8e521c515a90447305c5657f20f36ff3
Matheussmaced <mateusbyakuya3@gmail.com>
2024-10-02 22:26:21 -0300

    feat: use findAll controller

--- src/main/java/ecommerce/tshopping/example/ecommerce_tshopping/controller/UserController.java
+++ src/main/java/ecommerce/tshopping/example/ecommerce_tshopping/controller/UserController.java
@@ -1,17 +1,29 @@
 package ecommerce.tshopping.example.ecommerce_tshopping.controller;
 
+import java.util.List;
+
+import org.springframework.boot.autoconfigure.security.SecurityProperties.User;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import ecommerce.tshopping.example.ecommerce_tshopping.services.UsersService;
+
 @Controller
 @RequestMapping("/users")
 public class UserController {
+  private final UsersService usersService;
 
-  @RequestMapping("/{name}")
-  public ResponseEntity<String> test(@PathVariable String name) {
-    return ResponseEntity.ok("Nome: " + name);
+  public UserController(UsersService usersService) {
+    this.usersService = usersService;
+  }
+
+  @RequestMapping("/all")
+  public ResponseEntity<List<User>> findAll() {
+    var users = usersService.findAll();
+
+    return ResponseEntity.ok(users);
   }
 
 }
