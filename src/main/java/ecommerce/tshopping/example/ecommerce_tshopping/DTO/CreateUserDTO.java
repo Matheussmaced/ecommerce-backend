858e0cd98f79b386055333435d0728ce363cbc89
Matheussmaced <mateusbyakuya3@gmail.com>
2024-10-02 21:14:27 -0300

    feat: creating DTO user

---
+++ src/main/java/ecommerce/tshopping/example/ecommerce_tshopping/DTO/CreateUserDTO.java
@@ -0,0 +1,4 @@
+package ecommerce.tshopping.example.ecommerce_tshopping.DTO;
+
+public record CreateUserDTO(String name, String email, String password) {
+}
---
+++ src/main/java/ecommerce/tshopping/example/ecommerce_tshopping/DTO/UpdateUserDTO.java
@@ -0,0 +1,4 @@
+package ecommerce.tshopping.example.ecommerce_tshopping.DTO;
+
+public record UpdateUserDTO(String name, String email, String password) {
+}
